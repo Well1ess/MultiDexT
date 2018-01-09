d7a88c6f1862297057e5098e12cf2329127f5b58
WOWSHGA\shs1330 <shzhanglihua@corp.netease.com>
2018-01-09 18:52:11 +0800

    动态加载其他dex的第一步，无侵入的将我们能dex加载到Classloader的PathList的Elements数组中

---
+++ app/src/main/java/nim/shs1330/netease/com/multidext/Client.java
@@ -0,0 +1,41 @@
+package nim.shs1330.netease.com.multidext;
+
+import android.content.Context;
+
+import java.util.HashMap;
+
+/**
+ * Created by shs1330 on 2017/9/28.
+ */
+
+public class Client {
+    //功能类单例的管理类
+    private static Context context;
+    private static HashMap<Class<?>, Object> helpers = new HashMap<>();
+    public static void init(Context context){
+        Client.context = context;
+    }
+    public static <T> T getHelper(Class<T> tClass){
+        if (helpers == null){
+            helpers = new HashMap<>();
+        }
+
+        T target = (T) helpers.get(tClass);
+        if (target == null){
+            try {
+                target = tClass.newInstance();
+                helpers.put(tClass, target);
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return target;
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
--- app/src/main/java/nim/shs1330/netease/com/multidext/CustomApplication.java
+++ app/src/main/java/nim/shs1330/netease/com/multidext/CustomApplication.java
@@ -2,8 +2,17 @@ package nim.shs1330.netease.com.multidext;
 
 import android.app.Application;
 import android.content.Context;
-import android.os.Process;
 import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+
+import dalvik.system.DexClassLoader;
+import dalvik.system.DexFile;
 
 /**
  * Created by shs1330 on 2018/1/8.
@@ -15,6 +24,71 @@ public class CustomApplication extends Application {
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
         //MultiDex.install(base);
-        Log.d(TAG, "attachBaseContext: " + Process.myPid());
+        Client.init(base);
+
+        FileHelper.extractAssets("app-debug.apk");
+        File dexFile = getFileStreamPath("app-debug.apk");
+        File optDexFile = getFileStreamPath("app-debug.dex");
+
+        try {
+            pathClassLoader(dexFile, getClassLoader(), optDexFile);
+
+            Class c = Class.forName("nim.shs1330.netease.com.pluginone.MainActivity");
+            Object o = c.newInstance();
+
+            Log.d(TAG, "attachBaseContext: " + o.getClass());
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public static void pathClassLoader(File apkFile, ClassLoader cl, File optDexFile) throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, IOException, InvocationTargetException, InstantiationException {
+        Class baseDexClassLoaderC = DexClassLoader.class.getSuperclass();
+        Field pathListF = baseDexClassLoaderC.getDeclaredField("pathList");
+        pathListF.setAccessible(true);
+        Object pathListO = pathListF.get(cl);
+
+        //dexPathList类型
+        Class dexPathListC = pathListO.getClass();
+        Field dexElementsF = dexPathListC.getDeclaredField("dexElements");
+        dexElementsF.setAccessible(true);
+
+        Object[] dexElementsO = (Object[]) dexElementsF.get(pathListO);
+        //Element类型
+        Class elementC = dexElementsO.getClass().getComponentType();
+        //新数组
+        Object[] newElements = (Object[]) Array.newInstance(elementC, dexElementsO.length + 1);
+
+        //Element数组
+        Constructor<?> constructor = elementC.getConstructor(File.class, boolean.class, File.class, DexFile.class);
+        //插件中Apk的Element数组
+        Object o = constructor.newInstance(
+                apkFile,
+                false,
+                apkFile,
+                DexFile.loadDex(apkFile.getCanonicalPath(), optDexFile.getAbsolutePath(), 0));
+        Object[] toAddElementArray = new Object[] { o };
+
+        System.arraycopy(dexElementsO, 0, newElements, 0, dexElementsO.length);
+        System.arraycopy(toAddElementArray, 0, newElements, dexElementsO.length, toAddElementArray.length);
+
+        dexElementsF.set(pathListO, newElements);
     }
 }
