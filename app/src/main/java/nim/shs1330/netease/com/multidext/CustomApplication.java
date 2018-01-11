8778f5b874b2f3a5265e423df09a1860aef157fd
WOWSHGA\shs1330 <shzhanglihua@corp.netease.com>
2018-01-11 13:18:43 +0800

    dex测试

--- app/src/main/java/nim/shs1330/netease/com/multidext/CustomApplication.java
+++ app/src/main/java/nim/shs1330/netease/com/multidext/CustomApplication.java
@@ -2,17 +2,26 @@ package nim.shs1330.netease.com.multidext;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.support.multidex.MultiDex;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import dalvik.system.DexClassLoader;
 import dalvik.system.DexFile;
+
 
 /**
  * Created by shs1330 on 2018/1/8.
@@ -20,10 +29,57 @@ import dalvik.system.DexFile;
 
 public class CustomApplication extends Application {
     private static final String TAG = "CustomApplication";
+
+    private static Field findField(Object instance, String name) throws NoSuchFieldException {
+        Class clazz = instance.getClass();
+
+        while(clazz != null) {
+            try {
+                Field e = clazz.getDeclaredField(name);
+                if(!e.isAccessible()) {
+                    e.setAccessible(true);
+                }
+
+                return e;
+            } catch (NoSuchFieldException var4) {
+                clazz = clazz.getSuperclass();
+            }
+        }
+
+        throw new NoSuchFieldException("Field " + name + " not found in " + instance.getClass());
+    }
+
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        //MultiDex.install(base);
+        try {
+            ApplicationInfo info = base.getPackageManager().getApplicationInfo(base.getPackageName(), PackageManager.GET_META_DATA);
+            Log.d(TAG, "attachBaseContext: " + info.sourceDir);
+            File sourceApk = new File(info.sourceDir);
+            ZipInputStream zis = new ZipInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream(sourceApk)));
+            findField(base.getClassLoader(), "dexElementsSuppressedExceptions");
+            while (true) {
+                ZipEntry entry = zis.getNextEntry();
+                if ((entry == null)) {
+                    zis.close();
+                    break;
+                }
+                String name = entry.getName();
+                Log.d(TAG, "attachBaseContext: " + info.sourceDir + "下" + name);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+
+        MultiDex.install(base);
         Client.init(base);
 
         FileHelper.extractAssets("app-debug.apk");
